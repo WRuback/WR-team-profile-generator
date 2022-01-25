fcdddddbbcb9696bdb9e4751f517dcd63ddff9b4
William Ruback <rubackw@gmail.com>
2022-01-25 16:19:29 -0500

    Added Builder for the html, and builders for employee Cards.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
 
 const Employee = require("./lib/Employee.js");
 const Engineer = require("./lib/Engineer.js")
@@ -128,9 +129,110 @@ function writeToFile(fileName, data) {
             );
 }
 
+function buildHTML(teamArray) {
+    let teamBoxes = teamArray.map(function(element) {
+        if(element instanceof Manager){
+            return buildManager(element);
+        }
+        else if (element instanceof Engineer){
+            return buildEngineer(element);
+        }
+        else if (element instanceof Intern){
+            return buildIntern(element);
+        }
+    });
+    return `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+        <link rel="stylesheet" href="./assets/css/style.css" />
+        <script src="https://kit.fontawesome.com/48b5c6d0f7.js" crossorigin="anonymous"></script>
+        <title>Team Profile</title>
+    </head>
+    
+    <body>
+        <section class="hero is-danger is-fullheight">
+            <div class="hero-head has-background-info">
+                <p class="level-item has-text-centered title has-text-light p-5">Team Profile</p>
+            </div>
+            <div class="hero-body is-justify-content-center">
+                <div class="container columns is-multiline is-centered">
+                    ${teamBoxes.join("\n")}
+                </div>
+            </div>
+        </section>
+        <script src="https://use.fontawesome.com/a613337919.js"></script>
+    </body>
+    
+    </html>`
+}
+
+function buildManager(manager){
+    return `<div class="column is-one-third">
+    <div class="panel is-info">
+        <p class="panel-heading">
+            ${manager.getName()} - Manager <i class="fas fa-user-tie"></i>
+        </p>
+        <div class="panel-block has-background-white">
+            <p>ID: ${manager.getId()}</p>
+        </div>
+        <div class="panel-block has-background-white">
+            <p>Email: <a href="mailto: ${manager.getEmail()}" class="has-text-link">${manager.getEmail()}</a></p>
+        </div>
+        <div class="panel-block has-background-white">
+            <p>Office number: ${manager.getOfficeNumber()}</p>
+        </div>
+    </div>
+</div>`
+}
+
+function buildEngineer(engineer){
+    return `<div class="column is-one-third">
+    <div class="panel is-info">
+        <p class="panel-heading">
+            ${engineer.getName()} - Engineer <i class="fas fa-wrench"></i>
+        </p>
+        <div class="panel-block has-background-white">
+            <p>ID: ${engineer.getId()}</p>
+        </div>
+        <div class="panel-block has-background-white">
+            <p>Email: <a href="mailto: ${engineer.getEmail()}" class="has-text-link">${engineer.getEmail()}</a></p>
+        </div>
+        <div class="panel-block has-background-white">
+            <p>github: <a href="https://github.com/${engineer.getGithub()}" class="has-text-link" target="_blank">${engineer.getGithub()}</a></p>
+        </div>
+    </div>
+</div>`
+}
+
+function buildIntern(intern){
+    return `<div class="column is-one-third">
+    <div class="panel is-info">
+        <p class="panel-heading">
+            ${intern.getName()} - Intern <i class="fas fa-child"></i>
+        </p>
+        <div class="panel-block has-background-white">
+            <p>ID: ${intern.getId()}</p>
+        </div>
+        <div class="panel-block has-background-white">
+            <p>Email: <a href="mailto: ${intern.getEmail()}" class="has-text-link">${intern.getEmail()}</a></p>
+        </div>
+        <div class="panel-block has-background-white">
+            <p>School: ${intern.getSchool()}</p>
+        </div>
+    </div>
+</div>`
+}
+
 async function init() {
-    let teamArray = await questionLoop();
-    console.log(teamArray);
+    let currentTeam = await questionLoop();
+    console.log(currentTeam);
+    let teamProfile = buildHTML(currentTeam);
+    writeToFile("index_" + currentTeam[0].getName() + ".html", teamProfile);
 }
 
 init();
